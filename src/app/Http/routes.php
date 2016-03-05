71fe379bbe56bac963d536f70c5dc810d066187e
william bowman <gcphost@gmail.com>
2016-03-05 09:17:35 -0800

    token again

--- src/app/Http/routes.php
+++ src/app/Http/routes.php
@@ -11,9 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 /*
 |--------------------------------------------------------------------------
@@ -27,7 +24,9 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+  Route::get('/', function () {
+      return view('welcome');
+  });
 });
 
 
