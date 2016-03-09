35f2b6dcc3fcfeb9cdc62c7b37e718b9fd5644e3
william bowman <gcphost@gmail.com>
2016-03-09 14:48:13 -0800

    Migrations

---
+++ src/database/migrations/2016_03_09_223910_create_profiles_lookup_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesLookupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles_user', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('profile_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::drop('profiles_user');
+    }
+}
